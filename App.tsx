89402b8bf16203e2f163ea5521cd7463e9c20efc
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-04-20 13:49:58 +0430

    11. Add Fonts

--- App.tsx
+++ App.tsx
@@ -18,15 +18,15 @@ const App= ()=>{
 
   return (
     /* <View style={{flex:1, backgroundColor:'#333', justifyContent:'flex-start',flexDirection:'row-reverse'}}>*/
-    <View style={{flex:1, backgroundColor:'#333', justifyContent:'flex-start',flexDirection:'row'}}>
+    <View style={{flex:1, backgroundColor:'#333', justifyContent:'center',flexDirection:'row',alignItems:'center'}}>
 
      { /*<View  style={{flex:1, height:50, backgroundColor:'red'}}></View>
       <View  style={{flex:3,height:50, backgroundColor:'green'}}></View>*/}
-     { /*<Text style={{color:'#fff'}}>Hello React Native Developer </Text>*/}
+     <Text style={{color:'#fff',fontSize:18, fontFamily:'BYekan'}}>سلام آموزش React Native </Text>
 
-      <View  style={{flexGrow:1, flexShrink:1, flexBasis:100,  height:50, backgroundColor:'red'}}></View>
+      {/*<View  style={{flexGrow:1, flexShrink:1, flexBasis:100,  height:50, backgroundColor:'red'}}></View>
       <View  style={{flexGrow:2,flexShrink:2, flexBasis:200,height:50, backgroundColor:'green'}}></View>
-      <View  style={{flexGrow:3,flexShrink:0.5, flexBasis:300,height:50, backgroundColor:'blue'}}></View>
+    <View  style={{flexGrow:3,flexShrink:0.5, flexBasis:300,height:50, backgroundColor:'blue'}}></View>*/}
 
     </View>
     )
