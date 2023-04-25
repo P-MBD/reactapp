9a353549aa322d0589fce870b441195de03c8a0d
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-04-25 11:54:06 +0430

    14.state

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,14 @@
-import React from "react";
-import {View,Text} from 'react-native';
+import React, { useState } from "react";
+import {View,Text,TouchableOpacity} from 'react-native';
 const HomeScreen = (props) =>{
+    const[changeColor,setChangeColor]=useState(false)
     return(
         <View>
-            <Text style={{color:'#fff',fontSize:18, fontFamily:'BYekan'}}>{props.name} </Text>
+            {/*<Text style={{color:'#fff',fontSize:18, fontFamily:'BYekan'}}>{props.name} </Text>*/}
+            <View style={{width:50,height:50,backgroundColor: changeColor ? 'blue' : 'red'}}></View>
+            <TouchableOpacity onPress={()=>setChangeColor(!changeColor)}>
+                <Text>Click Me!</Text>
+            </TouchableOpacity>
         </View>
       )
 }
