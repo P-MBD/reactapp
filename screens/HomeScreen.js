3272db497f67917914d492854f987c082a0735be
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-04-25 14:08:36 +0430

    FlatList

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {View,Text,TouchableOpacity,ScrollView} from 'react-native';
+import {View,Text,TouchableOpacity,ScrollView,FlatList, LayoutAnimation} from 'react-native';
 const HomeScreen = (props) =>{
     const[changeColor,setChangeColor]=useState(false)
     const dataArray=[
@@ -8,36 +8,40 @@ const HomeScreen = (props) =>{
             family:'test11'
         },
         {
-            name:'test1',
-            family:'test11'
+            name:'test2',
+            family:'test22'
         },
         {
-            name:'test1',
-            family:'test11'
+            name:'test3',
+            family:'test33'
         },
         {
-            name:'test1',
-            family:'test11'
+            name:'test4',
+            family:'test44'
         },
         {
-            name:'test1',
-            family:'test11'
+            name:'test5',
+            family:'test55'
         },
         {
-            name:'test1',
-            family:'test11'
+            name:'test6',
+            family:'test66'
         },
         {
-            name:'test1',
-            family:'test11'
+            name:'test7',
+            family:'test77'
         },
         {
-            name:'test1',
-            family:'test11'
+            name:'test8',
+            family:'test88'
         },
         {
-            name:'test1',
-            family:'test11'
+            name:'test9',
+            family:'test99'
+        },
+        {
+            name:'test10',
+            family:'test1010'
         },
     ]
     return(
@@ -47,7 +51,7 @@ const HomeScreen = (props) =>{
             <TouchableOpacity onPress={()=>setChangeColor(!changeColor)}>
                 <Text>Click Me!</Text>
             </TouchableOpacity>*/}
-            <ScrollView horizontal={false} style={{marginVertical:20}} showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
+          {/*  <ScrollView horizontal={false} style={{marginVertical:20}} showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
                 {
                     dataArray.map((item, index)=>{
                         return(
@@ -60,7 +64,20 @@ const HomeScreen = (props) =>{
 
                     )
                 }
-            </ScrollView>
+            </ScrollView>*/}
+
+            <FlatList keyExtractor={(item,index)=>item.id + index} data={dataArray} renderItem={({item})=>{
+
+                return(
+                    <View>
+                        <Text >{item.id}</Text>
+                        <Text>{item.family}</Text>
+
+
+                    </View>
+                )
+
+            }} numColumns={2}/>
 
         </View>
       )
