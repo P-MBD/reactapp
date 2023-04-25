b689287700c31702322a51f3a16a0ffe0f93a7b2
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-04-25 12:52:26 +0430

    15.ScrollView

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,67 @@
 import React, { useState } from "react";
-import {View,Text,TouchableOpacity} from 'react-native';
+import {View,Text,TouchableOpacity,ScrollView} from 'react-native';
 const HomeScreen = (props) =>{
     const[changeColor,setChangeColor]=useState(false)
+    const dataArray=[
+        {
+            name:'test1',
+            family:'test11'
+        },
+        {
+            name:'test1',
+            family:'test11'
+        },
+        {
+            name:'test1',
+            family:'test11'
+        },
+        {
+            name:'test1',
+            family:'test11'
+        },
+        {
+            name:'test1',
+            family:'test11'
+        },
+        {
+            name:'test1',
+            family:'test11'
+        },
+        {
+            name:'test1',
+            family:'test11'
+        },
+        {
+            name:'test1',
+            family:'test11'
+        },
+        {
+            name:'test1',
+            family:'test11'
+        },
+    ]
     return(
-        <View>
-            {/*<Text style={{color:'#fff',fontSize:18, fontFamily:'BYekan'}}>{props.name} </Text>*/}
+        <View style={{backgroundColor:'#fff'}}>
+            {/*<Text style={{color:'#fff',fontSize:18, fontFamily:'BYekan'}}>{props.name} </Text>
             <View style={{width:50,height:50,backgroundColor: changeColor ? 'blue' : 'red'}}></View>
             <TouchableOpacity onPress={()=>setChangeColor(!changeColor)}>
                 <Text>Click Me!</Text>
-            </TouchableOpacity>
+            </TouchableOpacity>*/}
+            <ScrollView horizontal={false} style={{marginVertical:20}} showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
+                {
+                    dataArray.map((item, index)=>{
+                        return(
+                            <View key={index}>
+                                <Text >{item.name}</Text>
+                                <Text>{item.family}</Text>
+                            </View>
+                        )
+                    }
+
+                    )
+                }
+            </ScrollView>
+
         </View>
       )
 }
